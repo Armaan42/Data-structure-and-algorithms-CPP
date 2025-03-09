e85881cb08e70bb471ae559347623e393c9c2751
Armaan singh bhau <armaansinghbhau8@gmail.com>
2025-03-10 04:13:51 +0530

    perform addition on floating point numbers

---
+++ Day_2/problems/Ques_1.cpp
@@ -0,0 +1,12 @@
+// Modify the program to add two floating-point numbers and display the result.
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    float a,b;
+    cout << "Enter two floating-point numbers: ";
+    cin >> a >> b;
+
+    cout << "The sum of the two numbers is: " << a+b << endl;
+}
