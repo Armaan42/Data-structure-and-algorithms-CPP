a1b8e2cdffb56f28f6c806b59a0a9575feaec1a3
Armaan singh bhau <armaansinghbhau8@gmail.com>
2025-03-12 23:12:05 +0530

    #pattern4

---
+++ Day_3/patterns/number_pattern_1.cpp
@@ -0,0 +1,25 @@
+// print the below pattern
+
+//1 2 3 4 5 
+//2 3 4 5 
+//3 4 5 
+//4 5 
+//5 
+
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    int n;
+    cin >> n;
+
+    for(int i=1; i<=n; i++){
+        for(int j=i; j<=n; j++){
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
