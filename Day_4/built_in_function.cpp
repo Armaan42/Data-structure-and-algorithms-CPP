9e30efd4457f37bca09c16dfb3a69f4b360577cb
Armaan singh bhau <armaansinghbhau8@gmail.com>
2025-03-13 17:18:07 +0530

    built in function

---
+++ Day_4/built_in_function.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+
+int main(){
+    int a, b;
+    cin >> a >> b;
+
+    // cout << min(a, b) << endl;
+    // cout << max(a, b) << endl;
+    cout << sqrt(a) << endl;
+}
