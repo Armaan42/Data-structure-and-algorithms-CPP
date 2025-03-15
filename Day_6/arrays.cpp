540c782f559a783b1914924178a6a42878c6235e
Armaan singh bhau <armaansinghbhau8@gmail.com>
2025-03-15 09:42:35 +0530

    Initialized size

--- Day_6/arrays.cpp
+++ Day_6/arrays.cpp
@@ -3,11 +3,19 @@ using namespace std;
 
 int main(){
 
-    int arr[] = {1, 2, 3, 4, 5};
+    int size;
+    cin >> size;
+
+    int arr[size];
 
     for(int i=0; i<sizeof(arr)/sizeof(arr[0]); i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<size; i++){
         cout << arr[i] << " ";
     }
+
     cout << endl;
     return 0;
 }
