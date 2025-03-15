f7aaa793196c4c05d434c040b82a82edd0ecf5b7
Armaan singh bhau <armaansinghbhau8@gmail.com>
2025-03-15 10:11:56 +0530

    sum of array

---
+++ Day_6/sum_of_array.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int size;
+    cin >> size;
+
+    int arr[size];
+
+    int sum = 0;
+
+    for(int i=0; i<size; i++){
+        cin >> arr[i];
+    }
+
+    for(int i=0; i<size; i++){
+        sum += arr[i];
+    }
+    cout << sum << endl;
+}
