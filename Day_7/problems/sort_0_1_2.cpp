854ceda560f05fe2579f0fda318ad20df142eb08
Armaan singh bhau <armaansinghbhau8@gmail.com>
2025-03-18 19:06:41 +0530

    sort 0,1 and 2

---
+++ Day_7/problems/sort_0_1_2.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void sort123(vector<int> &v){
+    int noo = 0;
+    int noz = 0;
+    int notw = 0;
+
+    for(int i=0; i<v.size(); i++){
+        if(v[i] == 0){
+            noo++;
+        }
+        else if(v[i] == 1){
+            noz++;
+        }
+        else{
+            notw++;
+        }
+    }
+
+    for(int i=0; i<v.size(); i++){
+        if(i<noz){
+            v[i] = 0;
+        }
+        else if(i<(noo + noz)){
+            v[i] = 1;
+        }
+        else{
+            v[i] = 2;
+        }
+    }
+}
+
+int main(){
+
+    vector<int> v = {0,1,1,0,2,0,1,1,2,2,1,1,0,2,0};
+
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    sort123(v);
+
+    for(int i=0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
