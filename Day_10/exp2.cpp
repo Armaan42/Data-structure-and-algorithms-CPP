13e32156893b2b0985fab7b4d5ea51000d1f69c1
Armaan singh bhau <armaansinghbhau8@gmail.com>
2025-04-06 17:19:09 +0530

    Added some comments for easy understanding

--- Day_10/exp2.cpp
+++ Day_10/exp2.cpp
@@ -7,7 +7,7 @@ int main(){
     string str;
     getline(cin, str);
 
-    sort(str.begin(), str.end());
+    sort(str.begin(), str.end()); //sort always follow the ascii values
     cout << str << endl;
     return 0;
 }
