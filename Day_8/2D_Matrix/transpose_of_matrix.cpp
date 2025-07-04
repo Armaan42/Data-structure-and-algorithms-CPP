4ff29ce7c60037c6439ac050db40b75e14fb6b0c
Armaan <armaansinghbhau8@gmail.com>
2025-07-04 07:43:29 +0530

    transpose of the matrix

--- Day_8/2D_Matrix/transpose_of_matrix.cpp
+++ Day_8/2D_Matrix/transpose_of_matrix.cpp
@@ -1,13 +1,21 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+int main() {
+    int arr[2][3] = {{1, 2, 3}, {4, 5, 6}};
+    int store[3][2]; // Note: dimensions flipped for transpose
 
-    int arr[2][3] = {{1,2,3},{4,5,6}};
+    // Store the transpose
+    for(int i = 0; i < 2; i++) {
+        for(int j = 0; j < 3; j++) {
+            store[j][i] = arr[i][j]; // Correct assignment
+        }
+    }
 
-    for(int j=0; j<3; j++){
-        for(int i=0; i<2; i++){
-            cout << arr[i][j] << " ";
+    // Print the transposed matrix
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 2; j++) {
+            cout << store[i][j] << " ";
         }
         cout << endl;
     }
