6f771c856baac4e4d96ea3bc5b316d2ecc06108d
Armaan <armaansinghbhau8@gmail.com>
2025-07-09 11:37:07 +0530

    insert a character at a position

---
+++ Day_13/exp_4.cpp
@@ -0,0 +1,27 @@
+// insert a characeter at a position
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s;
+    getline(cin ,s);
+
+    char ch;
+    cin >> ch;
+
+    int pos;
+    cin >> pos;
+
+    if(pos >= 0 && pos<=s.length()){
+        s.insert(pos,1,ch);
+    }
+    else{
+        cout << "invalid positition" << endl;
+    }
+
+    cout << s << endl;
+
+    return 0;
+}
