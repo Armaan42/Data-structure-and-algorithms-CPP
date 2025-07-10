36245a278f54d203ae1f956a3940bf3bd5ed9df8
Armaan <armaansinghbhau8@gmail.com>
2025-07-10 17:38:08 +0530

    concatenating two string

---
+++ Day_13/exp_7.cpp
@@ -0,0 +1,13 @@
+// concatenating two string
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string s1,s2;
+    getline(cin ,s1);
+    getline(cin ,s2);
+
+    cout << s1+ " " + s2 << endl;
+}
