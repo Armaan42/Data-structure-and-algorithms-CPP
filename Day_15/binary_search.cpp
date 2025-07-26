9c61c80d1119e8207bc52d01e2da5c51b679bafe
Armaan <armaansinghbha8@gmail.com>
2025-07-27 01:42:34 +0530

    binary search

---
+++ Day_15/binary_search.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int binarySearch(int arr[], int target, int size){
+    int left = 0;
+    int right = size - 1;
+
+    while(left <= right){
+        int mid = ( left + right ) / 2;
+
+        if(arr[mid] == target){
+            return mid;
+        }
+        else if(arr[mid] > target){
+            right = mid - 1;
+        }
+        else{
+            left = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int size = sizeof(arr)/sizeof(arr[0]);
+
+    int target;
+    cin >> target;
+
+    int idx = binarySearch(arr, target, size);
+
+    if(idx == -1){
+        cout << "Element not found" << endl;
+    }
+    else{
+        cout << "Element found at index " << idx << endl;
+    }
+    return 0;
+}
