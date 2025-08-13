6d4adba844d1dc393e938ac0d2833569b1f17323
Armaan <armaansinghbha8@gmail.com>
2025-08-14 04:18:07 +0530

    pre-in-post

---
+++ Day_20/recursion_1/preInPost.cpp
@@ -0,0 +1,28 @@
+/*
+pre-in-post
+
+kaam = pre
+call1
+kaam = in
+call2
+kaam = post
+*/
+
+#include<iostream>
+using namespace std;
+
+void pip(int n){
+    if(n==0)return;
+    cout << "Pre : " << n << endl;
+    pip(n-1);
+
+    cout << "In : " << n << endl;
+    pip(n-1);
+
+    cout << "Post : " << n << endl;
+    pip(n-1);
+}
+
+int main(){
+    pip(3);
+}
