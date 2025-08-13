85830cc9449797f38b3527df4b2554d3a630c1b9
Armaan <armaansinghbha8@gmail.com>
2025-08-13 19:45:22 +0530

    WAP to calculate the nth fibnoacci number using recursion

---
+++ Day_20/recursion_1/exp_9.cpp
@@ -0,0 +1,19 @@
+/*
+WAP to calculate the nth fibnoacci number using recursion
+*/
+
+#include<iostream>
+using namespace std;
+
+int fibo(int n){
+    if(n==1 || n==2){
+        return 1;
+    }
+    return fibo(n-1) + fibo(n-2);
+}
+
+int main(){
+    cout << fibo(10);
+}
+
+ 
