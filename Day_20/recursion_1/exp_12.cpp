7d6023ceaaebb9478fcd2c982cf5d33de0050c61
Armaan <armaansinghbha8@gmail.com>
2025-08-13 21:40:36 +0530

    maze path

---
+++ Day_20/recursion_1/exp_12.cpp
@@ -0,0 +1,23 @@
+/*
+maze path:
+
+only two direction: right, down
+*/
+
+#include<iostream>
+using namespace std;
+
+int mazePath(int sr, int sc, int er, int ec){
+
+    if(sr > er || sc > ec) return 0;
+    if(sr == er || sc == ec) return 1;
+
+    int rightWays = mazePath(sr, sc+1, er, ec);
+    int downWays = mazePath(sr+1, sc, er, ec);
+    int totalWays = rightWays + downWays;
+    return totalWays;
+}
+
+int main(){
+    cout << mazePath(0,0,2,2) << endl;
+}
