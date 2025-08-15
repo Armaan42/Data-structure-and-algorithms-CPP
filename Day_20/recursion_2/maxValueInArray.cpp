a97d4058ae85694cf90db8ec7578cd969be943a3
Armaan <armaansinghbha8@gmail.com>
2025-08-15 15:35:50 +0530

    Find the maximum value in an array: 1. print the value; 2; store the value

--- Day_20/recursion_2/maxValueInArray.cpp
+++ Day_20/recursion_2/maxValueInArray.cpp
@@ -1,5 +1,7 @@
 /*
 find the max value in an array
+1. print
+2. store
 */
 
 #include<iostream>
@@ -17,11 +19,22 @@ void printMax(int arr[], int n, int idx, int max){
     printMax(arr, n, idx+1, max);
 }
 
+int maxInArray(int arr[], int n, int idx){
+
+    if(idx == n){
+        return INT_MIN;
+    }
+
+    return max(arr[idx], maxInArray(arr, n, idx+1));
+}
+
 int main(){
     int arr[] = {1,3,5,3,7,8};
     int n = sizeof(arr)/sizeof(arr[0]);
 
     int a = INT_MIN;
 
-    printMax(arr, n, 0, a);
+    // printMax(arr, n, 0, a);
+
+    cout << maxInArray(arr, n, 0) << endl;
 }
