837c8c142f003085476d6d295235e63df26af250
Armaan <armaansinghbha8@gmail.com>
2025-08-15 15:23:53 +0530

    Traversing an array and vector using recursion

---
+++ Day_20/recursion_2/printArray.cpp
@@ -0,0 +1,36 @@
+/*
+Traversing an array using recursion
+i.e., Print all the elements of an array
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void display(int arr[], int n, int idx){
+    if(idx == n) return;
+    cout << arr[idx] << " ";
+
+    display(arr, n, idx+1);
+}
+
+void display2(vector<int> &v, int idx){
+    if(idx == v.size()) return;
+
+    cout << v[idx] << " ";
+    display2(v, idx+1);
+}
+
+int main(){
+    int arr[] = {1,3,5,3,7,8};
+    int n = sizeof(arr)/sizeof(arr[0]);
+
+    vector<int> v(n);
+
+    for(int i=0; i<n; i++){
+        v[i] = arr[i];
+    }
+
+    // display(arr, n, 0);
+    display2(v, 0);
+}
