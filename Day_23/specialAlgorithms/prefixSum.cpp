9c6436fbea416ed347c3159d860932b2408ae4b1
Armaan <armaansinghbhau8@gmail.com>
2025-08-23 15:33:12 +0530

    Add another method to solve this question

--- Day_23/specialAlgorithms/prefixSum.cpp
+++ Day_23/specialAlgorithms/prefixSum.cpp
@@ -8,16 +8,31 @@
 #include<vector>
 using namespace std;
 
+// when you want prefix array store
+// int main(){
+//     vector<int> arr{1,4,5,3,2,7,6};
+//     int n = arr.size();
+//     vector<int> sum(n);
+
+//     sum[0] = arr[0];
+
+//     for(int i=0; i<n; i++){
+//         sum[i] = arr[i] + sum[i-1];
+//         cout << sum[i] << " ";
+//     }
+//     cout << endl;
+// }
+
+//when you want to print directly
 int main(){
     vector<int> arr{1,4,5,3,2,7,6};
     int n = arr.size();
-    vector<int> sum(n);
 
-    sum[0] = arr[0];
+    int prefixSum = 0;
 
     for(int i=0; i<n; i++){
-        sum[i] = arr[i] + sum[i-1];
-        cout << sum[i] << " ";
+        prefixSum = prefixSum + arr[i];
+        cout << prefixSum << " ";
     }
     cout << endl;
 }
